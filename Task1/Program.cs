58a7b1e03bb012d3f3d8cbdd790490af2bfabcf5
Sergey Antipov <antipovsergey84@gmail.com>
2023-07-04 19:10:46 +0300

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] ReadArray(string text)
+{
+    System.Console.WriteLine(text);
+    int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(" | ", array) + "]");
+}
+
+int PosSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+// |+|+|+|+|+|+||+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+||+|+|+|+|+|+|+|+|+|+|+|
+
+int[] array = ReadArray("Введите числа через пробел:");
+PrintArray(array);
+System.Console.WriteLine(PosSum(array));
---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+    void Intersection()
+    {
+        double k1;
+        double k2;
+        double b1;
+        double b2;
+        Console.WriteLine("Введите значение для k1:");
+        k1 = double.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Введите значение для k2:");
+        k2 = double.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Введите значение для b1:");
+        b1= double.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Введите значение для b2:");
+        b2 = double.Parse(Console.ReadLine()!);
+
+
+        if (k1 == k2)
+        {
+            System.Console.WriteLine("Точки пересечения не существует");
+        }
+        else
+        {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k2 * x + b2;
+        Console.WriteLine($"Координаты точки пересечения: ({x}, {y})");
+        }
+
+
+    }
+
+Intersection();
